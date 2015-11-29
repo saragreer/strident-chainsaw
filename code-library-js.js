89eba7d1e98b97e76151af4a6a3e9e0a4a2e593b
Sara Greer <sara.m.greer@gmail.com>
2015-11-29 15:06:24 -0600

    Generate random colors

---
+++ code-library-js.js
@@ -0,0 +1,28 @@
+
+/* ---- GENERATE RANDOMLY COLORED ITEMS ------------------------ */
+
+// Generate random RGB values
+function randomRGB() {
+  return Math.floor(Math.random() * 256);
+}
+
+// Generate three random RGB values to create a random color
+function randomColor() {
+  var color = 'rgb(';
+  color += randomRGB() + ',';
+  color += randomRGB() + ',';
+  color += randomRGB() + ')';
+  return color;
+}
+
+// Create numberOfElements items using randomRGB() and randomColor()
+var numberOfElements = 10;
+var rgbColor;
+var html = '';
+
+for (var i = 0; i < numberOfElements; i++) {
+  rgbColor = randomColor();
+  html =+ '<div style="background-color:' + rgbColor + '"></div>';
+}
+
+/* ---- SOURCE RANDOMLY COLORED ITEMS: teamtreehouse.com --------- */
